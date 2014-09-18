faede9275f6cce409dfffe5f98ea7a6a4e2e450f
Dh0mp5eur <dhomps.florian@gmail.com>
2014-09-18 11:44:13 +0200

    Add bashrc.srv

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 GITURL='https://raw.githubusercontent.com/Dh0mp5eur/Personal_Env/master/'
+input=0
 # Install BashRC
 echo '# Installer BashRC? [y/n]'
 read input
@@ -7,6 +8,15 @@ if [ $input == 'y' ]
 then curl "${GITURL}"bashrc > ~/.bashrc
 fi
 clear
+input=0
+# Install BashRC SRV
+echo '# Installer BashRC SRV? [y/n]'
+read input
+if [ $input == 'y' ]
+then curl "${GITURL}"bashrc.srv > ~/.bashrc
+fi
+clear
+input=0
 # Install VimRC and Solarized
 echo '# Installer VimRC? [y/n]'
 read input
@@ -17,3 +27,4 @@ mkdir ~/vim
 mkdir ~/vim/colors
 curl "${GITURL}"/vim/colors/solarized.vim > ~/vim/colors/solarized.vim
 clear
+input=0
